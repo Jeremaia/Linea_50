fdf8dd1ca544a3e5661c4ace96869f462f7c9b21
Jeremaia <37198184+Jeremaia@users.noreply.github.com>
2023-06-15 10:59:42 -0400

    preparacion_pagina

---
+++ Linea_50/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Linea50Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Linea_50'
--- base_pagina_Linea_50/Linea_50/models.py
+++
@@ -1,3 +0,0 @@
-from django.db import models
-
-# Nota: Models no es necesario en nuestro caso, debido a que estamos utilizando firebase
